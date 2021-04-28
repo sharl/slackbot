8a6c6225e044f3131cf11e3e2e117b4ddc985b45
Sharl Morlaroll <sharl@hauN.org>
2021-04-28 23:33:58 +0900

    config.jsonの拡張に対応 / サンプル追加

---
+++ modules/same_reaction.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+class call:
+    result = False
+
+    def __init__(self, item, sc=None, username='', icon_emoji='', channel=None, options=None):
+        # item {
+        #     'ts': '1619617321.005500',
+        #     'user': 'UGKNN7GD7',
+        #     'type': 'reaction_added',
+        #     'event_ts': '1619617321.005500',
+        #     'item_user': 'UGKNN7GD7',
+        #     'item': {
+        #         'ts': '1619617260.005400',
+        #         'channel': 'DGVNMT1EJ',
+        #         'type': 'message'
+        #     },
+        #     'reaction': 'thinking_face'
+        # }
+        if item['type'] == 'reaction_added':
+            if isinstance(options, list):
+                if item['reaction'] in options:
+                    print('options', options)
+                    data = {
+                        'channel': item['item']['channel'],
+                        'name': item['reaction'],
+                        'timestamp': item['item']['ts']
+                    }
+                    if sc:
+                        sc.api_call('reactions.add', **data)
+                    else:
+                        print(data)
+                    self.result = True
