eb3db0a424ce375a8bd60770b5bc00f024649ae6
Sharl Morlaroll <sharl@hauN.org>
2021-05-03 15:14:25 +0900

    キャッシュ機構分離 / モジュール拡張

--- hamu-bot.py
+++ hamu-bot.py
@@ -6,35 +6,19 @@ from __future__ import unicode_literals
 # import sys
 import os
 import time
-from datetime import datetime
-import re
-import json
-from urllib.parse import urlparse
 
 import slackclient
-
-import requests
-from bs4 import BeautifulSoup
 
 #
 # Slack bot setup
 #
-token = os.environ.get('SLACK_TOKEN', None)
-if not token:
-    print('SLACK_TOKEN is not set')
-    exit(1)
-
 username = 'hamu'
 icon_emoji = ':hamster:'
 
-# 問い合わせを減らすためのチャンネルIDキャッシュ
-channel_ids = {}
-# 問い合わせを減らすためのユーザIDキャッシュ
-user_ids = {}
 
+from modules import LoadModules, CACHES
 
-from modules import LoadModules
-
+caches = CACHES()
 modules = LoadModules()
 
 
@@ -49,56 +33,22 @@ def parse(sc, data):
         # detect user
         #
         user_id = item.get('user')
-        if isinstance(user_id, str) and user_id not in user_ids:
-            user = sc.api_call('users.info', user=user_id)
-            if user['ok'] is True:
-                user_name = user.get('user', {}).get('name')
-                if user_name is not None:
-                    user_ids[user_id] = user_name
+        caches.parseUID(sc, user_id)
         #
         # detect channel
         #
         channel_id = item.get('channel')
-        if isinstance(channel_id, str) and channel_id not in channel_ids:
-            if channel_id.startswith('C'):
-                # channel
-                chan = sc.api_call('conversations.info', channel=channel_id)
-                if chan['ok'] is True:
-                    channel_name = chan.get('channel', {}).get('name')
-                    if channel_name is not None:
-                        channel_ids[channel_id] = channel_name
-            elif channel_id.startswith('D'):
-                # im
-                user_name = user_ids.get(user_id, '???')
-                channel_ids[channel_id] = user_name
-            elif channel_id.startswith('G'):
-                # group im (mpim)
-                group = sc.api_call('groups.info', channel=channel_id)
-                if group['ok'] is True:
-                    channel_name = group.get('group', {}).get('name')
-                    if channel_name is not None:
-                        channel_ids[channel_id] = channel_name
-
-            if channel_id not in channel_ids:
-                channel_ids[channel_id] = channel_id
+        caches.parseCID(sc, channel_id, user_id)
         #
         # 拡張機能
         #
-        modules.call(item, sc=sc, username=username, icon_emoji=icon_emoji, channel=channel_id)
+        modules.call(item, sc=sc, username=username, icon_emoji=icon_emoji, channel=channel_id, user=user_id, caches=caches)
         #
         # parse message text
         #
         if item_type == 'message' and item.get('subtype', None) is None:
             text = item['text']
-            ts = item['ts']
             thread_ts = item.get('thread_ts')
-
-            ############################################################
-            print('{} {}> {}: {}'.format(datetime.fromtimestamp(float(ts)).strftime('%H:%M:%S'),
-                                         channel_ids[channel_id],
-                                         user_ids[user_id],
-                                         text))
-            ############################################################
 
             ############################################################
             # はむhelp
@@ -119,92 +69,12 @@ def parse(sc, data):
                 sc.api_call('chat.postMessage', **data)
                 time.sleep(1)
 
-            ############################################################
-            # はむをスレッドに呼ぶ
-            ############################################################
-            if text in ['はむ?', 'はむ？']:
-                if not thread_ts:
-                    data = {
-                        'username': username,
-                        'icon_emoji': icon_emoji,
-                        'channel': channel_id,
-                        'text': 'ほえ',
-                        'thread_ts': ts,
-                    }
-                    sc.api_call('chat.postMessage', **data)
-                    time.sleep(1)
-
-            # URI処理
-            urls = re.findall('https?://(?:[^\|]+?)(?:\|.*)?>', text)
-            print('urls', urls)
-            for url in urls:
-                url = url.rstrip('>')
-                if '|' in url and url.index('|') > 0:
-                    url = url.split('|')[0]
-                print('url', url)
-                scheme, netloc, path, params, query, fragment = urlparse(url)
-                ############################################################
-                # ようつべ、ニコニコ動画のURLをIRCに転送する
-                ############################################################
-                if netloc in ['www.youtube.com', 'm.youtube.com', 'youtu.be', 'www.nicovideo.jp', 'sp.nicovideo.jp', 'gyao.yahoo.co.jp']:
-                    ikachan = os.environ.get('IKACHAN', None)
-                    if channel_ids[channel_id] == 'ようつべ' and ikachan is not None:
-                        res = requests.post(ikachan, data={'channel': '#ようつべ',
-                                                           'message': '@{} {}'.format(user_ids[user_id], url),
-                                                           })
-                        data = {
-                            'username': username,
-                            'icon_emoji': icon_emoji,
-                            'channel': channel_id,
-                            'text': 'failed: {} : {}'.format(res, url),
-                        }
-                        if res.status_code == 200:
-                            data = {
-                                'username': username,
-                                'icon_emoji': icon_emoji,
-                                'channel': channel_id,
-                                'text': 'transfer done: {}'.format(url),
-                            }
-                        if thread_ts:
-                            data['thread_ts'] = thread_ts
-                        sc.api_call('chat.postMessage', **data)
-                        time.sleep(1)
-                if netloc.endswith('.2chan.net'):
-                    r = None
-                    try:
-                        r = requests.get(url, timeout=3)
-                    except Exception as e:
-                        print('exception', e)
-
-                    if r and r.status_code == 200:
-                        soup = BeautifulSoup(r.content, 'html.parser')
-                        img = None
-                        text = '\n'.join(soup.find('blockquote').strings)
-
-                        _as = soup.find_all('a')
-                        for a in _as:
-                            if a.get('href').startswith('/b/src/'):
-                                img = '/'.join(url.split('/')[:3]) + a.get('href')
-                                break
-                        if img:
-                            attachment = {
-                                'title_link': url,
-                                'text': text,
-                                'image_url': img,
-                            }
-                            data = {
-                                'username': username,
-                                'icon_emoji': icon_emoji,
-                                'channel': channel_id,
-                                'attachments': json.dumps([attachment]),
-                            }
-                            if thread_ts:
-                                data['thread_ts'] = thread_ts
-                            sc.api_call('chat.postMessage', **data)
-                            time.sleep(1)
-
-
 if __name__ == '__main__':
+    token = os.environ.get('SLACK_TOKEN', None)
+    if not token:
+        print('SLACK_TOKEN is not set')
+        exit(1)
+
     while True:
         sc = slackclient.SlackClient(token)
         try:
--- modules/__init__.py
+++ modules/__init__.py
@@ -18,8 +18,56 @@ class LoadModules:
         except Exception:
             pass
 
-    def call(self, item, sc=None, username='', icon_emoji='', channel=None):
-        for m in self.modules:
-            r = self.modules[m].call(item, sc=sc, username=username, icon_emoji=icon_emoji, channel=channel, options=self.options[m]).result
+    def call(self, item, sc=None, username='', icon_emoji='', channel=None, user=None, caches={}):
+        for m in sorted(self.modules):
+            r = self.modules[m].call(item, sc=sc, username=username, icon_emoji=icon_emoji, channel=channel, user=user, caches=caches, options=self.options[m]).result
             if r:
                 break
+
+
+class CACHES:
+    # 問い合わせを減らすためのチャンネルIDキャッシュ
+    channel_ids = {}
+    # 問い合わせを減らすためのユーザIDキャッシュ
+    user_ids = {}
+
+    def __init__(self):
+        pass
+
+    def parseUID(self, sc=None, user_id=None):
+        #
+        # detect user
+        #
+        if isinstance(user_id, str) and user_id not in self.user_ids:
+            user = sc.api_call('users.info', user=user_id)
+            if user['ok'] is True:
+                user_name = user.get('user', {}).get('name')
+                if user_name is not None:
+                    self.user_ids[user_id] = user_name
+
+    def parseCID(self, sc=None, channel_id=None, user_id=None):
+        #
+        # detect channel
+        #
+        if isinstance(channel_id, str) and channel_id not in self.channel_ids:
+            if channel_id.startswith('C'):
+                # channel
+                chan = sc.api_call('conversations.info', channel=channel_id)
+                if chan['ok'] is True:
+                    channel_name = chan.get('channel', {}).get('name')
+                    if channel_name is not None:
+                        self.channel_ids[channel_id] = channel_name
+            elif channel_id.startswith('D'):
+                # im
+                user_name = self.user_ids.get(user_id, '???')
+                self.channel_ids[channel_id] = user_name
+            elif channel_id.startswith('G'):
+                # group im (mpim)
+                group = sc.api_call('groups.info', channel=channel_id)
+                if group['ok'] is True:
+                    channel_name = group.get('group', {}).get('name')
+                    if channel_name is not None:
+                        self.channel_ids[channel_id] = channel_name
+
+            if channel_id not in self.channel_ids:
+                self.channel_ids[channel_id] = channel_id
