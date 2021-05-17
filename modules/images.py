c7f67b805c15352a87e075d1950081bf5766d0af
Sharl Morlaroll <sharl@hauN.org>
2021-05-17 14:15:17 +0900

    add images reaction

---
+++ modules/images.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import json
+from random import random
+import time
+
+
+class timer:
+    timer = 0
+
+
+class call:
+    result = False
+
+    def __init__(self, item, sc=None, username='', icon_emoji='', channel=None, user=None, caches={}, options=None):
+        if item['type'] == 'message' and item.get('subtype', None) is None:
+            text = item['text']
+            thread_ts = item.get('thread_ts')
+
+            now = time.time()
+            for k in options['keywords']:
+                if k in text and now - timer.timer > options['interval']:
+                    images = options['keywords'][k]
+                    attachment = {
+                        'title': '',
+                        'image_url': images[int(random() * len(images))],
+                    }
+                    data = {
+                        'username': username,
+                        'icon_emoji': icon_emoji,
+                        'channel': channel,
+                        'attachments': json.dumps([attachment]),
+                    }
+                    if thread_ts:
+                        data['thread_ts'] = thread_ts
+                    if sc:
+                        sc.api_call('chat.postMessage', **data)
+                    else:
+                        print(data)
+
+                    timer.timer = now
+                    return
