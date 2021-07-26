f0e2a5f575efcca3a1ddd49ed1ad30aa53338857
Sharl Morlaroll <sharl@hauN.org>
2021-07-26 22:25:27 +0900

    台風x号別表示追加

--- modules/typhoon.py
+++ modules/typhoon.py
@@ -11,35 +11,41 @@ class call:
     result = False
 
     def __init__(self, item, sc=None, username='', icon_emoji='', channel=None, user=None, caches={}, options=None):
-        typhoon_c0 = '台風広域'
-        typhoon_c1 = '台風'
-
+        url = 'https://typhoon.yahoo.co.jp/weather/jp/typhoon/'
+        typhoons = {
+            '台風広域': url,
+            '台風': url + '?c=1',
+        }
         if item['type'] == 'message' and item.get('subtype', None) is None:
             text = item['text']
             thread_ts = item.get('thread_ts')
-            url = 'https://typhoon.yahoo.co.jp/weather/jp/typhoon/'
+            r = requests.get(url, timeout=10)
+            if r and r.status_code == 200:
+                soup = BeautifulSoup(r.content, 'html.parser')
+                lis = soup.find_all('li', class_='tabView_item')
+                for l in lis[2:]:
+                    typhoons['台風' + l.a.text] = l.a['href']
 
-            if text in [typhoon_c0, typhoon_c1]:
-                if text == typhoon_c1:
-                    url += '?c=1'
+                if text in typhoons:
+                    url = typhoons[text]
 
-                r = requests.get(url, timeout=10)
-                if r and r.status_code == 200:
-                    soup = BeautifulSoup(r.content, 'html.parser')
-                    div = soup.find('div', class_='tabView_content_image')
-                    attachment = {
-                        'title': '',
-                        'image_url': div.img['src'],
-                    }
-                    data = {
-                        'username': text,
-                        'icon_emoji': icon_emoji,
-                        'channel': channel,
-                        'attachments': json.dumps([attachment]),
-                    }
-                    if thread_ts:
-                        data['thread_ts'] = thread_ts
-                    if sc:
-                        sc.api_call('chat.postMessage', **data)
-                    else:
-                        print(data)
+                    r = requests.get(url, timeout=10)
+                    if r and r.status_code == 200:
+                        soup = BeautifulSoup(r.content, 'html.parser')
+                        div = soup.find('div', class_='tabView_content_image')
+                        attachment = {
+                            'title': '',
+                            'image_url': div.img['src'],
+                        }
+                        data = {
+                            'username': text,
+                            'icon_emoji': icon_emoji,
+                            'channel': channel,
+                            'attachments': json.dumps([attachment]),
+                        }
+                        if thread_ts:
+                            data['thread_ts'] = thread_ts
+                        if sc:
+                            sc.api_call('chat.postMessage', **data)
+                        else:
+                            print(data)
